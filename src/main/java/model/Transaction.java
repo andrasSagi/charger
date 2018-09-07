b30f26302284dee8195ce23b2addf2e2886ebcd9
andrasSagi <andras.sagi8128@gmail.com>
2018-09-07 14:30:11 +0200

    Implement Transaction fields and constructor

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Transaction {
+import java.util.Calendar;
+
+class Transaction {
+
+    private TransactionType type;
+    private double amount;
+    private double balance;
+    private int year;
+    private int month;
+    private int day;
+
+    Transaction(TransactionType type, double amount, double balance) {
+        this.type = type;
+        this.amount = amount;
+        this.balance = balance;
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
 }
