fb8cf427c7a247f962970936b3ba1855a71501f4
andrasSagi <andras.sagi8128@gmail.com>
2018-09-07 13:44:59 +0200

    Implement User constructor and fields

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private double account;
+    private List<Transaction> transactions = new ArrayList<>();
+
+    public User(double account) {
+        this.account = account;
+    }
+
 }
