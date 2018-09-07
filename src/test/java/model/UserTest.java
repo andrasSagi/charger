2409155b8b0d1c3451cfea6bf0065a6d43c89e14
andrasSagi <andras.sagi8128@gmail.com>
2018-09-07 15:47:48 +0200

    Add test for withdraw

--- src/test/java/model/UserTest.java
+++ src/test/java/model/UserTest.java
@@ -1,7 +1,25 @@
 package model;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 public class UserTest {
 
+    private User testUser;
+
+    @Before
+    public void initializer() {
+        testUser = new User(new BigDecimal(1000));
+    }
+
+    @Test
+    public void testIfWithdrawDraws200From1000_ShouldBe800() {
+        testUser.withdraw(new BigDecimal(200));
+        assertEquals(new BigDecimal(800), testUser.getBalance());
+    }
 }
